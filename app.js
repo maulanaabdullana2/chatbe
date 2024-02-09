86f715d4b971defae5638d8e73e7ccd0489479d2
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-02-09 14:22:59 +0700

    first commit

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require('morgan')
 const app = express();
+const router = require("./router");
 
 app.use(cors());
 
+app.use(router)
+
 app.use(morgan('dev'))
-
-
-
 
 app.get("/", (req, res) => {
   res.status(200).json({
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
-const app = express();
-const router = require("./router");
-const createSocket = require("./socket/socket");
-const server = http.createServer(app);
+const Message = require("./models/MessagesModels");
+const { Server } = require("socket.io");
+const app = require("./app");
 require("dotenv").config();
-app.use(express.json());
+
+
 
 const database = process.env.DATABASE_URL;
 mongoose
@@ -18,31 +17,38 @@ mongoose
     console.error("Database connection error:", error);
   });
 
-  
+const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  });
 
-app.use(router);
+  io.on("connection", (socket) => {
+    console.log("INFO:", "seseorang telah bergabung ke chat room!");
 
+    socket.on("chat message", async (msg) => {
+      console.log("INFO:", "incoming message", JSON.stringify(msg));
+      io.emit("incoming message", msg);
 
+      // Simpan pesan ke dalam database
+      try {
+        await Message.create({
+          username: msg.username,
+          message: msg.message,
+        });
+      } catch (err) {
+        console.error("Error saving message to the database:", err);
+      }
+    });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  next();
+    socket.on("disconnect", () => {
+      console.log("INFO:", "seseorang telah pergi dari chat room!");
+    });
+  });
+server.listen(8000, () => {
+  console.log("INFO:", "Listening on port 8000");
 });
-
-createSocket(server);
-
-
-
-
-
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
-  console.log("INFO:", `Listening on port ${port}`);
-});
-
-
-
