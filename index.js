6cc41320a20635a1b54cbb9a8878cb51951b4860
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-02-28 02:06:38 +0700

    fix api upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `${"http://localhost:5173"}`,
+    origin: `${"https://chatfe.vercel.app/"}`,
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
