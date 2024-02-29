a4df5a0fa83e44830f442a09c8bcae85ece64e69
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-03-01 01:58:12 +0700

    make it better

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chatfe.vercel.app/",
+    origin: "https://chatfe.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
