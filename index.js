7b10c29cc88da7ca3edafe13fbde029bb3fd3ff8
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-03-01 01:14:31 +0700

    update url

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ io.on("connection", (socket) => {
       console.error("Kesalahan mendapatkan pesan dari database:", err);
     }
   });
-
   socket.on("disconnect", () => {
     console.log("INFO:", "Seseorang telah pergi dari chat room!");
   });
